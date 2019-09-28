59192ffb8820538fd62c9bbf9e9edb850bda317b
satoshi-maemoto <peugeot.106.s16@gmail.com>
2019-09-28 21:16:00 +0900

    Visualize the gyro rotation.

--- K4AUnityBT/Assets/K4AUnityBT/Samples/Scripts/ImuVisualizer.cs
+++ K4AUnityBT/Assets/K4AUnityBT/Samples/Scripts/ImuVisualizer.cs
@@ -7,15 +7,34 @@ public class ImuVisualizer : MonoBehaviour
     public GameObject deviceModel;
     public Text imuDataText;
 
+    private Vector3 integralGyro = Vector3.zero;
+    private ulong prevGyroTimestampUsec = 0;
+
     public void Apply(ImuSample value)
     {
+        if (this.prevGyroTimestampUsec == 0)
+        {
+            this.prevGyroTimestampUsec = value.gyroTimestampUsec;
+            return;
+        }
+
         var accValue = value.accSample;
-        this.imuDataText.text = $"IMU: TMP={value.temperature:0.0000} " +
-            $"ACC=({accValue.x:0.0000},{accValue.y:0.0000},{accValue.z:0.0000}) " +
-            $"GYRO=({value.gyroSample.x:0.0000},{value.gyroSample.y:0.0000},{value.gyroSample.z:0.0000})";
+        var gyroValue = value.gyroSample;
+        this.imuDataText.text = $"IMU: TMP={value.temperature:0.000} " +
+            $"ACC=({accValue.x:0.000},{accValue.y:0.000},{accValue.z:0.000}) " +
+            $"GYRO=({gyroValue.x:0.00},{gyroValue.y:0.00},{gyroValue.z:0.00})";
 
         var direction = new Vector3(-accValue.x, accValue.y, -accValue.z);
-        this.deviceModel.transform.localRotation = Quaternion.FromToRotation(-this.transform.up, direction);
+        var accRotation = Quaternion.FromToRotation(-this.transform.up, direction);
 
+        var timeDiff = (value.gyroTimestampUsec - this.prevGyroTimestampUsec) / 1000000f;
+        this.integralGyro += gyroValue * timeDiff;
+        gyroValue = this.integralGyro * Mathf.Rad2Deg;
+        var gyroRotation = Quaternion.Euler(-gyroValue);
+        this.prevGyroTimestampUsec = value.gyroTimestampUsec;
+
+        this.deviceModel.transform.localRotation = Quaternion.identity;
+        //this.deviceModel.transform.Rotate(accRotation.eulerAngles, Space.Self);
+        this.deviceModel.transform.Rotate(gyroRotation.eulerAngles, Space.Self);
     }
 }
